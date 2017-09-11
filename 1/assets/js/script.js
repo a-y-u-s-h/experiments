591775405cf97799d47e54f67c7852c4aea0188e
a-y-u-s-h <code.ayush@gmail.com>
2017-09-11 17:55:28 +0530

    Add Exp. 3 and Comments to Exp. 1

--- 1/assets/js/script.js
+++ 1/assets/js/script.js
@@ -1,19 +1,94 @@
-var particles = [];
-var smallCircles = [];
-var data = {
-  nsmall: 13,
-  diasmall: 30,
-  speed: 200,
-  clockwise: true,
-  alternatepaths: false,
-  showline: true,
-  diabig: 300,
-  showbigcircle: true,
-  showsmallcircle: true
-}
-var bigCircle;
-var controlkit;
+var particles = []; // Will store array of PointsOnCircle object instances.
+var smallCircles = []; // Will store array of CircularThing object instances : specifically speaking :- Smaller Circles.
+var bigCircle; // Variable to store Big Circle
 
+var data = {
+  nsmall: 13, // Number of Small Circles = Number of Points on those Small Circles.
+  diasmall: 30, // Diameter of Small Circles
+  diabig: 300, // DIameter of Big Circle
+  speed: 200, // Speed at which Points on Small Circles are rotating.
+  alternatepaths: false, // False if all Points rotate either clockwise or anticlockwise
+  clockwise: true, // True if Points rotate clockwise
+  showline: true, // True will display Lines
+  showbigcircle: true, // True will display Big Circle
+  showsmallcircle: true // True will display Small Circles
+};
+
+var controlkit; // Variable to store ControlKit
+function createControlKit() {
+  controlkit = new ControlKit();
+
+  controlkit
+    .addPanel({
+      label: "Control Panel",
+      width: 300,
+      fixed: false
+    })
+    .addNumberInput(data, "nsmall", {
+      label: "Small Circles ",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addNumberInput(data, "diasmall", {
+      label: "Radius (small) ",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addNumberInput(data, "diabig", {
+      label: "Radius (Big) ",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addNumberInput(data, "speed", {
+      label: "Speed",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addCheckbox(data, "clockwise", {
+      label: "Clockwise?",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addCheckbox(data, "alternatepaths", {
+      label: "Alternate Spin?",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addCheckbox(data, "showline", {
+      label: "Show lines?",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addCheckbox(data, "showbigcircle", {
+      label: "Show Big Circle?",
+      onChange: function() {
+        createCircles();
+      }
+    })
+    .addCheckbox(data, "showsmallcircle", {
+      label: "Show Small Circles?",
+      onChange: function() {
+        createCircles();
+      }
+    });
+}
+
+/*
+  Will create a circle with center  (cx, cy), radius : r.
+  (x, y) are coordinates of any point on that circle.
+  Therefore : 
+  x = rcos(theta + phi);
+  y = rsin(theta + phi);
+  The 'angle' that is to be passed will be used by 'incrementTheta' method.
+  'incrementTheta' : Will increment 'theta' field by 'angle' amount and update x and y.
+*/
 function CircularThing(cx, cy, r, angle = 0, phi = 0) {
   this.cx = cx;
   this.cy = cy;
@@ -26,17 +101,29 @@ function CircularThing(cx, cy, r, angle = 0, phi = 0) {
     stroke(0);
     noFill();
     strokeWeight(1);
+    // Drawing out a circle with (cx, cy) as its center and r as its radius.
     ellipse(this.cx, this.cy, this.r, this.r);
-    this.x = this.cx + (this.r * Math.cos(this.theta + this.phi));
-    this.y = this.cy + (this.r * Math.sin(this.theta));
-  }
+
+    // Setting up coordinates of arbitrary point on this circle, with theta = 0 initially;
+    this.x = this.cx + this.r * Math.cos(this.theta + this.phi);
+    this.y = this.cy + this.r * Math.sin(this.theta);
+  };
   this.incrementTheta = function() {
+    // Increasing theta by 'angle' amount, which is by default = 0.
     this.theta += angle;
-    this.x = this.cx + (this.r * Math.cos(this.theta + this.phi));
-    this.y = this.cy + (this.r * Math.sin(this.theta + this.phi));
-  }
+    // Updating x and y fields so that they can be used somewhere.
+    this.x = this.cx + this.r * Math.cos(this.theta + this.phi);
+    this.y = this.cy + this.r * Math.sin(this.theta + this.phi);
+  };
 }
 
+/*
+  Arguments to be passed : 
+  (cx, cy) : Coordinates of Center of Circle on which you want this point to lie.
+  r : Radius of Circle on which you want this point to lie.
+  clockwise : If true, the point will rotate in clockwise direction.
+  phi : phase for the point. By default = 0
+*/
 function PointOnCircle(cx, cy, r, clockwise = true, phi = 0) {
   this.r = r;
   this.cx = cx;
@@ -50,111 +137,78 @@ function PointOnCircle(cx, cy, r, clockwise = true, phi = 0) {
   this.show = function() {
     stroke(0);
     strokeWeight(1);
+    this.x = this.cx + this.r * Math.cos(this.theta + this.phi);
+    this.y = this.cy + this.r * Math.sin(this.theta + this.phi);
     point(this.x, this.y);
-  }
+  };
+
   this.update = function() {
-    this.x = this.cx + (this.r * Math.cos(this.theta + this.phi));
-    this.y = this.cy + (this.r * Math.sin(this.theta + this.phi));
+    this.x = this.cx + this.r * Math.cos(this.theta + this.phi);
+    this.y = this.cy + this.r * Math.sin(this.theta + this.phi);
     if (clockwise) {
       this.theta += this.speed;
     } else {
       this.theta -= this.speed;
     }
-  }
-
+  };
 }
 
 function createCircles() {
+  /* 
+    passed arguments to big circle : cx, cy, r, angle, phi = 0
+    Notice that we are passing it (0, 0) to be the center, reason being, we'll shift everything to center of screen later in draw.
+  */
   bigCircle = new CircularThing(0, 0, data.diabig, 2 * PI / data.nsmall);
-
   bigCircle.theta = 2 * PI / data.nsmall;
+  /*
+    Since you can't create private fields in JS, it makes sense to just update them when you want without creating setter method, no?
+    Here we created a big circle, now we're setting its theta value to be controlled by our GUI.
+    When number of small circles = 10, theta will be 2*PI/10. We'll put a small circle on that coordinate
+    Then we'll update our theta, and update (x, y) of an arbitrary point on our bigCircle, and then put another small circle.
+    In the process creating what we desire => Small circles on big circle.
+  */
   smallCircles = [];
   particles = [];
+
   for (var i = 0; i < data.nsmall; i += 1) {
     bigCircle.incrementTheta();
-    smallCircles[i] = new CircularThing(bigCircle.x / 2, bigCircle.y / 2, data.diasmall);
-    particles[i] = new PointOnCircle(smallCircles[i].cx, smallCircles[i].cy, data.diasmall / 2, data.clockwise, i);
+    smallCircles[i] = new CircularThing(
+      bigCircle.x / 2,
+      bigCircle.y / 2,
+      data.diasmall
+    );
+    /*
+    Just exactly how we put Small Circles on Big Circle, I'll put points on Small Circles.
+    */
+    particles[i] = new PointOnCircle(
+      smallCircles[i].cx,
+      smallCircles[i].cy,
+      data.diasmall / 2,
+      data.clockwise,
+      i
+    );
+    // Logic to handle when alternatepaths field is changed in our data object.
     if (data.alternatepaths) {
       if (i % 2 == 0) {
         particles[i].speed *= -1;
       }
     }
-    console.log(bigCircle.theta);
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  controlkit = new ControlKit();
-
-  controlkit.addPanel({
-      label: 'Control Panel',
-      width: 300,
-      fixed: false
-    })
-    .addNumberInput(data, 'nsmall', {
-      label: 'Small Circles ',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addNumberInput(data, 'diasmall', {
-      label: 'Radius (small) ',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addNumberInput(data, 'diabig', {
-      label: 'Radius (Big) ',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addNumberInput(data, 'speed', {
-      label: 'Speed',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addCheckbox(data, 'clockwise', {
-      label: 'Clockwise?',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addCheckbox(data, 'alternatepaths', {
-      label: 'Alternate Spin?',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addCheckbox(data, 'showline', {
-      label: 'Show lines?',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addCheckbox(data, 'showbigcircle', {
-      label: 'Show Big Circle?',
-      onChange: function() {
-        createCircles();
-      }
-    })
-    .addCheckbox(data, 'showsmallcircle', {
-      label: 'Show Small Circles?',
-      onChange: function() {
-        createCircles();
-      }
-    })
-
+  createControlKit();
   createCircles();
 }
 
 function draw() {
   background(255);
-  translate(windowWidth / 2, windowHeight / 2);
-  if (data.showbigcircle) {
 
+  // Shifting origin to center of screen as promised.
+  translate(windowWidth / 2, windowHeight / 2);
+
+  if (data.showbigcircle) {
     bigCircle.show();
   }
   strokeWeight(1);
@@ -174,7 +228,6 @@ function draw() {
         if (data.showline) {
           line(particles[i].x, particles[i].y, particles[j].x, particles[j].y);
         }
-
       }
     }
   }
---
+++ 3/assets/js/script.js
@@ -0,0 +1,108 @@
+/* 
+	Data related to this experiment: 
+	n = number of stars,
+	r  = radius of those stars (max),
+	speed = speed of stars,
+	stars = an array containing those stars
+*/
+var data = {
+	n: 800,
+	r: 4,
+	speed: 100,
+	stars: []
+};
+
+/*
+Class : Star 
+Methods : show, update
+Fields : x, y, z, pz
+
+x : current value of x coordinate of star object instance
+y : current value of y coordinate of star object instance
+z : current value of z coordinate of star object instance
+pz : previous value of z coordinate of star object instance (before next .update() is called)
+
+*/
+class Star {
+	constructor() {
+		this.x = random(-width, width);
+		this.y = random(-height, height);
+		this.z = random(width);
+		this.pz = this.z;
+	}
+
+	show() {
+		fill(255);
+		noStroke();
+
+		/*
+			Instead of updating x and y, we'll update our z field.
+			It's not really our z coordinate.
+			z will decrease from some value (max : 'width' value ) to <=1
+			x and y will increase from their positions to outside the canvas as z <= 1
+			if z is less than one, x and y will reach their approx max, taking line / star outside the canvas.
+			we'll then update our z back to some random number from 0 to width.
+		*/
+		let sx = map(this.x / this.z, 0, 1, 0, width);
+		let sy = map(this.y / this.z, 0, 1, 0, height);
+
+		// Uncomment below lines if you want circles instead of lines
+		// let r = map(this.z, 0, width, data.r, 0);
+		// ellipse(sx, sy, r, r);
+
+		/*
+			px and py will store previous values of x and y;
+			previous value of z is pz for now (at least before we make this.pz and this.z equal just below declaring px and py)
+		*/
+		let px = map(this.x / this.pz, 0, 1, 0, width);
+		let py = map(this.y / this.pz, 0, 1, 0, height);
+		/*
+			Now, we set pz to be current value of z, after this when update is called, pz will again become previous value
+			..till our control flow reaches this statement.
+		*/
+		this.pz = this.z;
+		strokeWeight(1);
+		stroke(255);
+		line(px, py, sx, sy);
+	}
+
+	update() {
+		/*
+			this.z decreases by some pixel value everytime update is called in draw.
+			as a result it'll some day become less than 1.
+			When that happens, our Star will go outside the canvas.
+			We want our star back with its position and stuff reinitialized. Hence...
+		*/
+		this.z = this.z - data.speed;
+		if (this.z < 1) {
+			this.z = width;
+			this.pz = this.z;
+			this.x = random(-width, width);
+			this.y = random(-height, height);
+		}
+	}
+}
+
+setup = () => {
+	createCanvas(windowWidth, windowHeight);
+	/*
+		Putting Star object instances in stars array.
+	*/
+	if (data.n) {
+		for (var i = 0, upperLimit = data.n; i < upperLimit; i += 1) {
+			data.stars.push(new Star());
+		}
+	}
+};
+
+draw = () => {
+	background(0);
+	translate(width / 2, height / 2);
+
+	data.speed = map(mouseX, 0, width, 2, 80);
+
+	for (var i = 0, upperLimit = data.n; i < upperLimit; i += 1) {
+		data.stars[i].show();
+		data.stars[i].update();
+	}
+};
