dda34e6dad3eb6af938b8151fa96070c6dc0ea98
a-y-u-s-h <code.ayush@gmail.com>
2017-09-23 00:31:56 +0530

    Use production link in Exp. 14

--- 14/assets/js/script.js
+++ 14/assets/js/script.js
@@ -5,7 +5,7 @@ let loaded = false;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // song = loadSound("/sounds/experiments/14.mp3", soundloaded);
-  song = loadSound("https://rawgit.com/a-y-u-s-h/sounds/master/experiments/14.mp3", soundloaded);
+  song = loadSound("https://cdn.rawgit.com/a-y-u-s-h/sounds/master/experiments/14.mp3", soundloaded);
   fft = new p5.FFT(0, 64);
 }
 
