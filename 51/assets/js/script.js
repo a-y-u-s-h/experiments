7756ed23dd7dc33f1cf67607127fad08eb044d13
a-y-u-s-h <code.ayush@gmail.com>
2017-10-29 23:44:00 +0530

    Add Exp. 51

--- 51/assets/js/script.js
+++ 51/assets/js/script.js
@@ -1,74 +1,104 @@
 let data = {
   sketch: {
-    background: "#FFFFFF"
+    background: "#333444"
   },
-  turtle: {
-    len: 10,
-    length_multiplier: 0.5,
-    stroke: {
-      weight: 1,
-      color: "#000000"
-    },
-    rotation: {
-      left: 30,
-      right: 30
-    }
+  pixels: {
+    size: 20
   }
 };
 
-function turtle(sentence, len) {
-  background(data.sketch.background);
-  resetMatrix();
-  strokeWeight(data.turtle.stroke.weight);
-  stroke(data.turtle.stroke.color);
-  angleMode(DEGREES);
-  translate(width * 0.5, height);
+let grid;
 
-  for (var i = 0, upperLimit_i = sentence.length; i < upperLimit_i; i += 1) {
-    let current = sentence.charAt(i);
-    switch (current) {
-      case "w":
-      case "W":
-        line(0, 0, 0, -len);
-        translate(0, -len);
-        break;
-      case "s":
-      case "S":
-        line(0, 0, 0, len);
-        translate(0, len);
-        break;
-      case "d":
-      case "D":
-        rotate(data.turtle.rotation.right);
-        line(0, 0, 0, -len);
-        translate(0, -len);
-        break;
-      case "a":
-      case "A":
-        rotate(-data.turtle.rotation.left);
-        line(0, 0, 0, -len);
-        translate(0, -len);
-        break;
-      case "p":
-      case "P":
-        push();
-        break;
-      case "o":
-      case "O":
-        pop();
-        break;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 100);
+  rectMode(CENTER);
+  grid = new Grid();
+  grid.init();
+}
+
+function draw() {
+  background(data.sketch.background);
+  grid.disco();
+  grid.show();
+  console.log(frameCount);
+}
+
+class Pixel {
+  constructor(cx, cy, i = 0, j = 0) {
+    this.cx = cx;
+    this.cy = cy;
+    this.i = i;
+    this.j = j;
+    this.color = color(random(100), random(100), random(100));
+  }
+
+  show() {
+    push();
+    fill(this.color);
+    strokeWeight(1 + 2 * sin(frameCount * 0.01))
+    translate(this.cx, this.cy);
+    rotate(frameCount * 0.05)
+    rect(0, 0, data.pixels.size, data.pixels.size);
+    pop();
+  }
+
+  disco()  {
+    this.color = color(20 + 20 * sin(frameCount * 0.01) + random(10, 20), random(10, 20) + 50 + 20 * cos(frameCount * (this.j + 1) * 0.0001), 100);
+  }
+}
+
+class Grid {
+  constructor() {
+    this.pixels = [];
+  }
+
+  init() {
+    for (
+      let i = 0, upperLimit_i = round(width / data.pixels.size);
+      i < upperLimit_i;
+      i += 1
+    ) {
+      this.pixels[i] = [];
+      for (
+        let j = 0, upperLimit_j = round(height / data.pixels.size);
+        j < upperLimit_j;
+        j += 1
+      ) {
+        let x = (i + 0.5) * data.pixels.size;
+        let y = (j + 0.5) * data.pixels.size;
+        this.pixels[i].push(new Pixel(x, y, i, j));
+      }
+    }
+  }
+
+  show() {
+    for (
+      let i = 0, upperLimit_i = this.pixels.length;
+      i < upperLimit_i;
+      i += 1
+    ) {
+      for (
+        let j = 0, upperLimit_j = this.pixels[i].length;
+        j < upperLimit_j;
+        j += 1
+      ) {
+        this.pixels[i][j].show();
+      }
+    }
+  }
+
+  destroy()  {
+    this.pixels = [];
+  }
+
+
+  disco()  {
+    for (var i = 0, upperLimit_i = this.pixels.length ; i < upperLimit_i; i += 1 ) {
+      for (var j = 0, upperLimit_j = this.pixels[i].length ; j < upperLimit_j; j += 1 ) {
+        this.pixels[i][j].disco();
+      }
     }
   }
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {}
-
-let sentence = "";
-function keyTyped() {
-  sentence += `${key}`;
-  turtle(sentence, 10);
-}
