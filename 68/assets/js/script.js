cb177f0ffaba18093868363bc82d1d4e63e6c8c3
a-y-u-s-h <code.ayush@gmail.com>
2017-11-15 23:44:19 +0530

    Add Exp. 68

--- 68/assets/js/script.js
+++ 68/assets/js/script.js
@@ -1,52 +1,151 @@
 let data = {
   sketch: {
     background: "#FFFFFF"
-  },
-  tree: {
-    n : 14,
-    root: {
-      x: window.innerWidth * 0.5,
-      y: window.innerHeight * 0.5
-    },
-    node: {
-      size: 100,
-      textSize: 40,
-      textColor: "#FFFFFF",
-      stroke: {
-        check: true,
-        color: "#000000"
-      },
-      fill: {
-        check: true,
-        color: "#000000"
-      }
-    },
-    branch: {
-      r: 150,
-      angle: {
-        left: 45,
-        right: 45
-      }
-    }
   }
 };
 
-let tree;
+let separator;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
-  angleMode(DEGREES);
-
-  tree = new Tree(width * 0.5, height * 0.1);
-  for (var i = 0, upperLimit_i = data.tree.n ; i < upperLimit_i; i += 1 ) {
-    tree.put(round(random(0, 100)));
-  }
+  separator = new Separator();
+  noFill();
 }
 
 function draw() {
-  background(data.sketch.background);
-  tree.connect();
-  tree.traverse();
+  background(255);
+
+  if (frameCount % 10 == 0) {
+    if (mouseX < width * 0.5) {
+      separator.init();
+    }
+  }
+  separator.show();
 }
+
+class Separator {
+  constructor(x = width * 0.5) {
+    this.x = x;
+    this.slits = [new Slit(x, height * 0.335), new Slit(x, height * 0.685)];
+    this.waves = [];
+  }
+
+  init() {
+    this.waves.push(new Bar());
+  }
+
+  takeout() {
+    this.waves.forEach((wave, index) => {
+      if (wave.position.x > width * 0.25) {
+        this.waves.splice(index, 1);
+      }
+      this.waves.clean(undefined);
+      this.waves.clean(null);
+    });
+  }
+
+  show() {
+    push();
+    strokeWeight(5);
+    strokeCap(SQUARE);
+    line(width * 0.5, 0, width * 0.5, height * 0.32);
+    line(width * 0.5, height * 0.35, width * 0.5, height * 0.67);
+    line(width * 0.5, height * 0.7, width * 0.5, height);
+    pop();
+
+    this.takeout();
+
+    if (this.waves.length > 0) {
+      this.waves.forEach(wave => {
+        wave.update();
+        wave.show();
+      });
+    }
+
+    this.slits.forEach(slit => {
+      if (frameCount % 5 == 0) {
+        if (this.waves.length > 0) {
+          if (this.waves[0].position.x >= width * 0.24) {
+            slit.insert();
+          }
+        }
+      }
+      slit.emit();
+    });
+  }
+}
+
+class Slit {
+  constructor(x, y) {
+    this.position = new p5.Vector(x, y);
+    this.semicircles = [];
+  }
+
+  insert() {
+    this.semicircles.push(new Semicircle(this.position.x, this.position.y));
+  }
+
+  takeout() {
+    this.semicircles.forEach((semicircle, index) => {
+      if (semicircle.r > height * 2) {
+        this.semicircles.splice(index, 1);
+      }
+      this.semicircles.clean(undefined);
+      this.semicircles.clean(null);
+    });
+  }
+
+  emit() {
+    this.semicircles.forEach(semicircle => {
+      semicircle.update();
+      semicircle.show();
+      this.takeout();
+    });
+  }
+}
+
+class Semicircle {
+  constructor(cx, cy) {
+    this.position = new p5.Vector(cx, cy);
+    this.r = 0;
+  }
+
+  show() {
+    push();
+    fill(0, 3);
+    translate(this.position.x, this.position.y);
+    arc(0, 0, this.r * 2, this.r * 2, -HALF_PI, HALF_PI);
+    pop();
+  }
+
+  update() {
+    this.r = lerp(this.r, this.r + 5, 0.7);
+  }
+}
+
+class Bar {
+  constructor() {
+    this.position = new p5.Vector();
+  }
+
+  show() {
+    push();
+    translate(this.position.x, this.position.y);
+    line(this.position.x, 0, this.position.x, height);
+    pop();
+  }
+
+  update() {
+    this.position.x = lerp(this.position.x, this.position.x + 5, 0.6);
+  }
+}
+
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
