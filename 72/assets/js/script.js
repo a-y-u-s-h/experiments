af4c24d88c122449a709165dfe9fcf22a9badcd1
a-y-u-s-h <code.ayush@gmail.com>
2017-11-19 23:33:29 +0530

    Add Exp. 72

--- 72/assets/js/script.js
+++ 72/assets/js/script.js
@@ -1,52 +1,121 @@
-let data = {
+let settings = {
   sketch: {
-    background: "#FFFFFF"
+    background: "#BBBBBB"
   },
-  tree: {
-    n : 14,
-    root: {
-      x: window.innerWidth * 0.5,
-      y: window.innerHeight * 0.5
+  data: {
+    draw: {
+      onmousedragged: true
     },
-    node: {
-      size: 100,
-      textSize: 40,
-      textColor: "#FFFFFF",
-      stroke: {
-        check: true,
-        color: "#000000"
-      },
-      fill: {
-        check: true,
-        color: "#000000"
-      }
+    size: 10,
+    fill: {
+      check: true,
+      color: "#222222"
     },
-    branch: {
-      r: 150,
-      angle: {
-        left: 45,
-        right: 45
-      }
+    stroke: {
+      check: false,
+      color: "#000000",
+      weight: 1
     }
+  },
+  line: {
+    m: 1,
+    b: 0,
+    weight: 2,
+    color: "#000000"
   }
 };
 
-let tree;
+let data = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
-  angleMode(DEGREES);
-
-  tree = new Tree(width * 0.5, height * 0.1);
-  for (var i = 0, upperLimit_i = data.tree.n ; i < upperLimit_i; i += 1 ) {
-    tree.put(round(random(0, 100)));
-  }
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("#container");
+  canvas.id("canvas");
 }
 
 function draw() {
-  background(data.sketch.background);
-  tree.connect();
-  tree.traverse();
+  background(settings.sketch.background);
+
+  for (var i = 0, upperLimit_i = data.length; i < upperLimit_i; i += 1) {
+    let x = map(data[i].x, 0, 1, 0, width);
+    let y = map(data[i].y, 0, 1, 0, height);
+    showData(x, y);
+  }
+
+  if (data.length > 1) {
+    linearRegression();
+    drawLine();
+  }
 }
+
+var linearRegression = () => {
+  let xsum = 0;
+  let ysum = 0;
+  for (var i = 0, upperLimit_i = data.length; i < upperLimit_i; i += 1) {
+    xsum += data[i].x;
+    ysum += data[i].y;
+  }
+
+  let xmean = xsum / data.length;
+  let ymean = ysum / data.length;
+
+  let num = 0;
+  let den = 0;
+
+  for (var i = 0, upperLimit_i = data.length; i < upperLimit_i; i += 1) {
+    let x = data[i].x;
+    let y = data[i].y;
+    num += (x - xmean) * (y - ymean);
+    den += (x - xmean) * (x - xmean);
+  }
+
+  settings.line.m = num / den;
+  settings.line.b = ymean - settings.line.m * xmean;
+};
+
+var drawLine = () => {
+  let x1 = 0;
+  let y1 = settings.line.m * x1 + settings.line.b;
+  let x2 = 1;
+  let y2 = settings.line.m * x2 + settings.line.b;
+
+  x1 = map(x1, 0, 1, 0, width);
+  y1 = map(y1, 0, 1, 0, height);
+  x2 = map(x2, 0, 1, 0, width);
+  y2 = map(y2, 0, 1, 0, height);
+  push();
+  stroke(settings.line.color);
+  strokeWeight(settings.line.weight);
+  line(x1, y1, x2, y2);
+  pop();
+};
+
+var showData = (x, y) => {
+  if (settings.data.fill.check) {
+    fill(settings.data.fill.color);
+  } else {
+    noFill();
+  }
+
+  if (settings.data.stroke.check) {
+    stroke(settings.data.stroke.color);
+    strokeWeight(settings.data.stroke.weight);
+  } else {
+    noStroke();
+  }
+
+  ellipse(x, y, settings.data.size);
+};
+
+  var mouseDragged = () => {
+    let x = map(mouseX, 0, width, 0, 1);
+    let y = map(mouseY, 0, height, 0, 1);
+    let point = createVector(x, y);
+    data.push(point);
+  };
+  var mousePressed = () => {
+    let x = map(mouseX, 0, width, 0, 1);
+    let y = map(mouseY, 0, height, 0, 1);
+    let point = createVector(x, y);
+    data.push(point);
+  };
--- 75/assets/js/script.js
+++ 75/assets/js/script.js
@@ -1,13 +1,52 @@
 let data = {
   sketch: {
     background: "#FFFFFF"
+  },
+  tree: {
+    n : 14,
+    root: {
+      x: window.innerWidth * 0.5,
+      y: window.innerHeight * 0.5
+    },
+    node: {
+      size: 100,
+      textSize: 40,
+      textColor: "#FFFFFF",
+      stroke: {
+        check: true,
+        color: "#000000"
+      },
+      fill: {
+        check: true,
+        color: "#000000"
+      }
+    },
+    branch: {
+      r: 150,
+      angle: {
+        left: 45,
+        right: 45
+      }
+    }
+  }
+};
+
+let tree;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+
+  tree = new Tree(width * 0.5, height * 0.1);
+  for (var i = 0, upperLimit_i = data.tree.n ; i < upperLimit_i; i += 1 ) {
+    tree.put(round(random(0, 100)));
   }
 }
 
-function setup () {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw () {
+function draw() {
   background(data.sketch.background);
+  tree.connect();
+  tree.traverse();
 }
