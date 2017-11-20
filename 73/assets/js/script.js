790193a4875a9f889a38639a0b8e597ce60f1503
a-y-u-s-h <code.ayush@gmail.com>
2017-11-20 23:21:54 +0530

    Add Exp. 73

--- 73/assets/js/script.js
+++ 73/assets/js/script.js
@@ -1,23 +1,122 @@
 let data = {
   sketch: {
-    background: "#248B21"
+    background: "#000000"
+  },
+  mickey: {
+    number: 64,
+    head: 10
   }
 };
 
-let ground;
-let left_paddle;
-let right_paddle;
+let mickeys = [];
+let song;
+let fft;
+let amp;
 
-function setup() {
+var loaded = () => {
+  song.loop();
+};
+
+var setup = () => {
   createCanvas(windowWidth, windowHeight);
-  left_paddle = new Paddle("left");
-  right_paddle = new Paddle("right");
-  ground = new Ground(width * 0.5, height * 0.5, left_paddle);
-}
+  song = loadSound("/sounds/experiments/73.mp3", loaded);
+  fft = new p5.FFT(0.8, 64);
+  amp = new p5.Amplitude();
 
-function draw() {
+  for (var i = 0, upperLimit_i = data.mickey.number; i < upperLimit_i; i += 1) {
+    mickeys[i] = new Mickey(i);
+  }
+  angleMode(DEGREES);
+  ellipseMode(CENTER);
+  colorMode(HSB, 100);
+  noStroke();
+};
+
+var draw = () => {
   background(data.sketch.background);
-  ground.show();
-  left_paddle.show();
-  right_paddle.show();
+  let vol = amp.getLevel();
+  let spectrum = fft.analyze();
+
+  for (
+    var i = spectrum.length - 1, upperLimit_i = 0;
+    i > upperLimit_i;
+    i -= 1
+  ) {
+    mickeys[i].show(1, spectrum[i]);
+  }
+};
+
+class Mickey {
+  constructor(i = 1, cx = width * 0.5, cy = height * 0.6) {
+    this.i = i;
+    this.origin = new p5.Vector(cx, cy);
+    this.x = 0;
+    this.y = 0;
+  }
+
+  show(vol = 1, f = 0) {
+    push();
+    translate(this.origin.x, this.origin.y);
+    translate(this.x, this.y);
+    if (this.i % 2 == 0) {
+      fill(
+        map(f, 0, 255, 0, 50 + 50 * sin(frameCount)),
+        100,
+        100
+      );
+    } else {
+      fill(0);
+    }
+    this.shape(map(vol * f, 0, 1000, 0, 30));
+    pop();
+  }
+
+  shape(size = 1) {
+    push();
+    ellipse(
+      0,
+      0,
+      data.mickey.head * size * this.i,
+      data.mickey.head * size * this.i
+    );
+
+    // Left most nail
+    push();
+    translate(
+      -data.mickey.head * size * this.i * 0.5,
+      -data.mickey.head * size * this.i * 0.5
+    );
+    ellipse(
+      0,
+      0,
+      data.mickey.head * size * this.i * 0.7,
+      data.mickey.head * size * this.i * 0.7
+    );
+    pop();
+
+    // Right most nail
+    push();
+    translate(
+      data.mickey.head * size * this.i * 0.5,
+      -data.mickey.head * size * this.i * 0.5
+    );
+    ellipse(
+      0,
+      0,
+      data.mickey.head * size * this.i * 0.7,
+      data.mickey.head * size * this.i * 0.7
+    );
+    pop();
+
+    pop();
+  }
+
+  update() {
+    let rand = random(1);
+    if (rand < 0.25) {
+    } else if (rand < 0.5) {
+    } else if (rand < 0.75) {
+    } else {
+    }
+  }
 }
