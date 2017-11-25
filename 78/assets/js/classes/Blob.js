e207e229315c5c5d2d61152e6ecfce5943a02b11
a-y-u-s-h <code.ayush@gmail.com>
2017-11-25 23:00:12 +0530

    Add Exp. 78

---
+++ 78/assets/js/classes/Blob.js
@@ -0,0 +1,26 @@
+class Blob {
+    constructor(cx, cy) {
+        this.position = new p5.Vector(cx, cy);
+        this.velocity = p5.Vector.random2D();
+        this.r = random(200, 400);
+    }
+
+    show() {
+        push();
+        translate(this.position.x, this.position.y);
+        ellipse(0, 0, this.r * 2, this.r * 2);
+        pop();
+    }
+
+    update() {
+        this.position.add(this.velocity);
+
+        if (this.position.x > width || this.position.x < 0) {
+            this.velocity.x *= -1;
+        }
+
+        if (this.position.y > height || this.position.y < 0) {
+            this.velocity.y *= -1;
+        }
+    }
+}
--- 78/assets/js/script.js
+++ 78/assets/js/script.js
@@ -1,157 +1,76 @@
 let data = {
-  sketch: {
-    background: "#000000",
-    frameRate: 60
-  },
-  snake: {
-    size: 10,
-    fill: "#53D921",
-    stroke: {
-      color: "#00550A",
-      weight: 1
+    sketch: {
+        background: "#FFFFFF",
+        scale: window.innerHeight / window.innerWidth,
+        width: 160,
+        height: 120
+    },
+    metaball: {
+        number: 8,
+        size: 0.5
     }
-  },
-  food: {
-    n: 30,
-    types: [
-      {
-        color: "#DBB822",
-        probability_of_showing_up: 0.2,
-        score: 3
-      },
-      {
-        color: "#DB1BA6",
-        probability_of_showing_up: 0.5,
-        score: 2
-      },
-      {
-        color: "#258197",
-        probability_of_showing_up: 0.7,
-        score: 1
-      },
-      {
-        color: "#BD1A1A",
-        probability_of_showing_up: 0.4,
-        score: -2
-      }
-    ]
-  },
-  grid: {
-    color: "#0B150B"
-  }
 };
 
-// Variable to store GUI
-var controlkit;
-
-// Function to create control GUI
-var createControlKit = () => {
-  controlkit = new ControlKit();
-  controlkit
-    .addPanel({
-      fixed: true,
-      align: "right",
-      label: "Game Settings"
-    })
-    .addColor(data.grid, "color", {
-      colorMode: "hex",
-      label: "Grid Color"
-    })
-    .addNumberInput(data.snake, "size", {
-      label: "Grid Size",
-      step: 1,
-      dp: 1
-    })
-    .addNumberInput(data.sketch, "frameRate", {
-      label: "Framerate",
-      step: 1,
-      dp: 1
-    })
-    .addSubGroup({
-      label: "Snake Settings"
-    })
-    .addColor(data.snake, "fill", {
-      colorMode: "hex",
-      label: "Fill Color"
-    })
-    .addColor(data.snake.stroke, "color", {
-      colorMode: "hex",
-      label: "Stroke Color"
-    })
-    .addNumberInput(data.snake.stroke, "weight", {
-      label: "Border Thickness",
-      step: 1,
-      dp: 1
-    });
-};
-
-createControlKit();
-
-let snake;
-let grid;
-let foods = [];
-
-function init_food(snake, grid) {
-  for (var i = 0, upperLimit_i = data.food.n; i < upperLimit_i; i += 1) {
-    foods.push(new Food(snake, grid));
-    foods[i].initialize();
-  }
-}
+let blobs = [];
 
 function setup() {
-  createCanvas(windowWidth * 0.77, windowHeight);
-  grid = new Grid();
-  snake = new Snake();
-  background(color(data.sketch.background));
-  init_food(snake, grid);
+    let canvas = createCanvas(
+        data.sketch.width,
+        data.sketch.height * data.sketch.scale
+    );
+    canvas.id("canvas");
+    colorMode(RGB, 100);
+
+    for (
+        var i = 0, upperLimit_i = data.metaball.number;
+        i < upperLimit_i;
+        i += 1
+    ) {
+        blobs.push(new Blob(random(width), random(height)));
+    }
 }
 
-let angle = 0;
 function draw() {
-  frameRate(abs(data.sketch.frameRate));
-  background(0, 100);
-  let spacingX =
-    (width - Math.floor(width / data.snake.size) * data.snake.size) * 0.5;
-  let spacingY =
-    (height - Math.floor(height / data.snake.size) * data.snake.size) * 0.5;
-  translate(spacingX, spacingY);
-  grid.show();
-  snake.update();
-  snake.show();
-  for (var i = 0, upperLimit_i = foods.length; i < upperLimit_i; i += 1) {
-    if (foods[i]) {
-      foods[i].update();
-      foods[i].show();
+    background(data.sketch.background);
 
-      if (foods[i].cx == snake.cx && foods[i].cy == snake.cy) {
-        snake.eats(foods[i]);
-        foods.splice(i, 1);
-        foods[i] = new Food(snake, grid);
-        foods[i].initialize();
-      }
+    loadPixels();
+    for (var x = 0, upperLimit_x = width; x < upperLimit_x; x += 1) {
+        for (var y = 0, upperLimit_y = height; y < upperLimit_y; y += 1) {
+            let index = (x + y * width) * 4;
+            let sum = 0;
+            blobs.forEach(b => {
+                let d = dist(x, y, b.position.x, b.position.y);
+                sum += b.r / d ;
+            });
+            let col = color(
+                map(
+                    sum,
+                    0,
+                    (width + height) * data.metaball.size,
+                    0,
+                    100 * sin(frameCount * 0.1)
+                ),
+                map(
+                    sum,
+                    0,
+                    (width + height) * data.metaball.size,
+                    0,
+                    100 * sin(frameCount * 0.05)
+                ),
+                map(
+                    sum,
+                    0,
+                    (width + height) * data.metaball.size,
+                    10,
+                    100 * sin(frameCount * 0.01)
+                )
+            );
+            set(x, y, col);
+        }
     }
-  }
-}
+    updatePixels();
 
-function keyPressed() {
-  if (mouseX < width) {
-    switch (keyCode) {
-      case UP_ARROW:
-        snake.dir(0, -1);
-        break;
-      case DOWN_ARROW:
-        snake.dir(0, 1);
-        break;
-      case LEFT_ARROW:
-        snake.dir(-1, 0);
-        break;
-      case RIGHT_ARROW:
-        snake.dir(1, 0);
-        break;
-    }
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth * 0.77, windowHeight);
+    blobs.forEach(b => {
+        b.update();
+    });
 }
