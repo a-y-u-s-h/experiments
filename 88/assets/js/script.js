dfb264afa03bfbcd08371409d9e9e3a0e0ce2d6d
a-y-u-s-h <code.ayush@gmail.com>
2017-12-05 20:51:22 +0530

    Add Exp. 88

--- 88/assets/js/script.js
+++ 88/assets/js/script.js
@@ -1,23 +1,55 @@
 let data = {
   sketch: {
-    background: "#248B21"
+    background: "#FFFFFF78"
   }
 };
 
-let ground;
-let left_paddle;
-let right_paddle;
+let video;
+let loading = true;
+let scaleX;
+let scaleY;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  left_paddle = new Paddle("left");
-  right_paddle = new Paddle("right");
-  ground = new Ground(width * 0.5, height * 0.5, left_paddle);
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.id("canvas");
+  noStroke();
+  video = createVideo("assets/media/img/88.mp4", loaded);
+  video.size(80, 60);
+  video.hide();
+  textAlign(CENTER, CENTER);
+  colorMode(HSB, 100);
+  scaleX = Math.ceil(width / 80);
+  scaleY = Math.ceil(height / 60);
+}
+
+function loaded() {
+  loading = false;
+  if (!loading) {
+    video.loop();
+  }
 }
 
 function draw() {
   background(data.sketch.background);
-  ground.show();
-  left_paddle.show();
-  right_paddle.show();
+  if (!loading) {
+    video.loadPixels();
+
+    for (var x = 0, upperLimit_x = video.width; x < upperLimit_x; x += 1) {
+      for (var y = 0, upperLimit_y = video.height; y < upperLimit_y; y += 1) {
+        let index = (x + y * video.width) * 4;
+        let r = video.pixels[index + 0];
+        let g = video.pixels[index + 1];
+        let b = video.pixels[index + 2];
+
+        fill(r, g, b);
+        if (r > g) {
+          push();
+          translate(x * scaleX, y * scaleY);
+          text("##", 0, 0);
+          pop(); 
+        }
+      }
+    }
+    updatePixels();
+  }
 }
