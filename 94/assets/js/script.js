b5c227a946f8bcd3e04152a1c2820825f5cedc28
a-y-u-s-h <code.ayush@gmail.com>
2017-12-11 01:57:47 +0530

    Add Exp. 94

--- 94/assets/js/script.js
+++ 94/assets/js/script.js
@@ -1,52 +1,68 @@
 let data = {
   sketch: {
-    background: "#FFFFFF"
+    background: "#BC1E00"
   },
-  tree: {
-    n : 14,
-    root: {
-      x: window.innerWidth * 0.5,
-      y: window.innerHeight * 0.5
-    },
-    node: {
-      size: 100,
-      textSize: 40,
-      textColor: "#FFFFFF",
-      stroke: {
-        check: true,
-        color: "#000000"
-      },
-      fill: {
-        check: true,
-        color: "#000000"
-      }
-    },
-    branch: {
-      r: 150,
-      angle: {
-        left: 45,
-        right: 45
-      }
-    }
+  squiggly: {
+    n: 50,
+    np: 50,
+    separation: 18,
+    amplitude: 20,
+    inner_radius: 30
   }
 };
 
-let tree;
+let things = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
-  angleMode(DEGREES);
 
-  tree = new Tree(width * 0.5, height * 0.1);
-  for (var i = 0, upperLimit_i = data.tree.n ; i < upperLimit_i; i += 1 ) {
-    tree.put(round(random(0, 100)));
+  angleMode(DEGREES);
+  noFill();
+  stroke(255);
+  strokeWeight(1);
+  for (var i = 0, upperLimit_i = data.squiggly.n; i < upperLimit_i; i += 1) {
+    things[i] = new Squiggly(width * 0.5, height * 0.5, i);
   }
 }
 
 function draw() {
-  background(data.sketch.background);
-  tree.connect();
-  tree.traverse();
+  background(188, 30, 0, 100);
+
+  for (var i = 0, upperLimit_i = data.squiggly.n; i < upperLimit_i; i += 1) {
+    things[i].show();
+  }
+
+  let radius = 2 * (data.squiggly.inner_radius + data.squiggly.separation);
+  push();
+  noStroke();
+  fill(255, 100);
+  translate(width * 0.5, height * 0.5);
+  ellipse(0, 0, radius, radius);
+  pop();
+}
+
+class Squiggly {
+  constructor(cx, cy, i = 0) {
+    this.position = new p5.Vector(cx, cy);
+    this.i = i + 1;
+    this.rotation = 360 / data.squiggly.n * (i + 1);
+  }
+
+  show() {
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(this.rotation);
+    beginShape();
+    for (var i = 0, upperLimit_i = data.squiggly.np; i < upperLimit_i; i += 1) {
+      let x = data.squiggly.inner_radius + data.squiggly.separation * i;
+      let y =
+        data.squiggly.amplitude *
+        Math.log(i) *
+        cos(frameCount * 5 + (i + 1) * data.squiggly.amplitude);
+      vertex(x, y);
+    }
+    endShape();
+    pop();
+  }
+
 }
