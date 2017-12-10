11b6b138d3cde317e29ad32d4f5066fdcc87b788
a-y-u-s-h <code.ayush@gmail.com>
2017-12-10 17:31:06 +0530

    Add Exp. 93

--- 93/assets/js/script.js
+++ 93/assets/js/script.js
@@ -1,23 +1,57 @@
 let data = {
   sketch: {
-    background: "#248B21"
+    background: "#FFFFFF"
+  },
+  bodies: {
+    n: 50,
+    spacing: 10,
+    base_radius: 0
   }
 };
 
-let ground;
-let left_paddle;
-let right_paddle;
+let bodies = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  left_paddle = new Paddle("left");
-  right_paddle = new Paddle("right");
-  ground = new Ground(width * 0.5, height * 0.5, left_paddle);
+  for (var i = 1, upperLimit_i = data.bodies.n; i <= upperLimit_i; i += 1) {
+    bodies.push(new Body(width * 0.5, height * 0.5, i));
+  }
+  noStroke();
+  ellipseMode(CENTER);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(data.sketch.background);
-  ground.show();
-  left_paddle.show();
-  right_paddle.show();
+  background(1);
+  for (var i = bodies.length - 1, upperLimit_i = 0; i > upperLimit_i; i -= 1) {
+    if (i % 2) {
+      fill(255);
+    } else {
+      fill(0);
+    }
+    bodies[i].show();
+    bodies[i].update();
+  }
+}
+
+class Body {
+  constructor(x, y, i = 1) {
+    this.x = x;
+    this.y = y;
+    this.i = i;
+    this.r = this.i * data.bodies.spacing + data.bodies.base_radius;
+  }
+
+  show() {
+    push();
+    rect(this.x, this.y, this.r, this.r);
+    pop();
+  }
+
+  update() {
+    this.x =
+      100 * tan(frameCount * (data.bodies.n - this.i) * 0.0002) * sin(frameCount * (data.bodies.n - this.i) * 0.002) + width * 0.5;
+    this.y =
+      100 * cos(frameCount * (data.bodies.n - this.i) * 0.001) + height * 0.5;
+  }
 }
