8bf62d2dbc4f73bf37cdad47d6c0a024560a6373
a-y-u-s-h <code.ayush@gmail.com>
2017-12-28 22:03:23 +0530

    Add Exp. 111

--- 111/assets/js/script.js
+++ 111/assets/js/script.js
@@ -1,52 +1,61 @@
 let data = {
   sketch: {
-    background: "#FFFFFF"
+    background: "#000000"
   },
-  tree: {
-    n : 14,
-    root: {
-      x: window.innerWidth * 0.5,
-      y: window.innerHeight * 0.5
+  perlin: {
+    offsets: {
+      x: 0,
+      y: 0,
+      increment: 0.006
     },
-    node: {
-      size: 100,
-      textSize: 40,
-      textColor: "#FFFFFF",
-      stroke: {
-        check: true,
-        color: "#000000"
-      },
-      fill: {
-        check: true,
-        color: "#000000"
-      }
-    },
-    branch: {
-      r: 150,
-      angle: {
-        left: 45,
-        right: 45
-      }
-    }
-  }
-};
-
-let tree;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
-  angleMode(DEGREES);
-
-  tree = new Tree(width * 0.5, height * 0.1);
-  for (var i = 0, upperLimit_i = data.tree.n ; i < upperLimit_i; i += 1 ) {
-    tree.put(round(random(0, 100)));
+    increment: 1,
+    start_size: 1,
+    size_multiplier: 1.02
   }
 }
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  blendMode(ADD);
+  noFill();
+  stroke(random(255), random(255), random(255), 128);
+}
+
 function draw() {
-  background(data.sketch.background);
-  tree.connect();
-  tree.traverse();
+  translate(width * 0.5, height * 0.5);
+
+  if (data.perlin.start_size < 3000) {
+    // Create a series of perlin rings from big to small
+    for (var i = 0; i < 10; i++) {
+
+      // Less points for smaller rings
+      nPoints = int(2 * PI * data.perlin.start_size);
+      nPoints = min(nPoints, 800);
+
+      // Create ring
+      beginShape();
+      for (var i = 0; i < nPoints; i++) {
+        var a = i / nPoints * TAU;
+        var p = p5.Vector.fromAngle(i / nPoints * TAU);
+        var n = noise(data.perlin.offsets.x + p.x * data.perlin.increment, data.perlin.offsets.y + p.y * data.perlin.increment) * data.perlin.start_size;
+        p.mult(n);
+        vertex(p.x, p.y);
+      }
+      endShape(CLOSE);
+
+      // Increment perlin offset for next ring
+      data.perlin.offsets.x += data.perlin.offsets.increment;
+      data.perlin.offsets.y += data.perlin.offsets.increment;
+
+      // Update size
+      data.perlin.start_size *= data.perlin.size_multiplier;
+    }
+  }
+}
+
+function mousePressed () {
+  blendMode(NORMAL);
+  background(0);
+  blendMode(ADD);
 }
