5a1b626fe4ff4f0927ceebbd1d66ab217543a582
a-y-u-s-h <code.ayush@gmail.com>
2018-01-06 23:40:28 +0530

    Add Exp. 120

--- 120/assets/js/script.js
+++ 120/assets/js/script.js
@@ -1,91 +1,63 @@
 let data = {
   sketch: {
-    background: "#FFFFFF01"
-  },
-  circle: {
-    r: 200,
-    spacing: 5,
-    toMorphto: -3
+    background: "#000000"
   }
 };
 
-let circlePath = [];
-let trianglePath = [];
-
-function polarToCartesian(r, angle) {
-  return createVector(r * cos(angle), r * sin(angle));
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  background(data.sketch.background);
-  strokeWeight(1);
-
-  let startA = 0;
-  let endA = (360 / data.circle.toMorphto);
-
-  let startV = polarToCartesian(data.circle.r, startA);
-  let endV = polarToCartesian(data.circle.r, endA);
-
-  for (var a = startA; a < 360; a += data.circle.spacing) {
-    let x = data.circle.r * cos(a);
-    let y = data.circle.r * sin(a);
-    let cv = new p5.Vector(x, y);
-    circlePath.push(cv);
-
-    let amountToLerp = (a % (360 / data.circle.toMorphto)) / (endA - startA);
-    let tv = p5.Vector.lerp(startV, endV, amountToLerp);
-    trianglePath.push(tv);
-
-    if ( (a + data.circle.spacing) % (360 / data.circle.toMorphto) == 0) {
-      startA = startA + (360 / data.circle.toMorphto);
-      endA = endA + (360 / data.circle.toMorphto);
-      startV = polarToCartesian(data.circle.r, startA);
-      endV = polarToCartesian(data.circle.r, endA);
-    }
-  }
+  // colorMode(HSB, 100);
 }
 
 function draw() {
   background(data.sketch.background);
-  translate(width * 0.5, height * 0.5);
-  rotate(30 + frameCount);
-  stroke(0);
-  noFill();
-  // beginShape();
-  // circlePath.forEach(p => {
-  //   vertex(p.x, p.y);
-  //   push();
-  //   strokeWeight(3);
-  //   point(p.x, p.y);
-  //   pop();
-  // });
-  // endShape(CLOSE);
+  orbitControl();
 
-  // beginShape();
-  // trianglePath.forEach(p => {
-  //   vertex(p.x, p.y);
-  //   push();
-  //   strokeWeight(3);
-  //   point(p.x, p.y);
-  //   pop();
-  // });
-  // endShape(CLOSE);
-  let amt = (sin(frameCount * 3) + 2) * 0.5;
-  stroke("#00000050")
-  beginShape();
-  for ( var i = 0 ; i < circlePath.length; i += 1 ) {
-    let cv = circlePath[i];
-    let tv = trianglePath[i];
+  ambientLight(60);
 
-    let x = lerp(cv.x, tv.x, amt);
-    let y = lerp(cv.y, tv.y, amt);
-    vertex(x, y);
-    // push();
-    // strokeWeight(5);
-    // point(x, y);
-    // pop();
+  push();
+  let x1 = 300 * sin(frameCount);
+  let y1 = 400 * sin(frameCount * 2);
+  let z1 = 500 * sin(frameCount * 0.5);
+  pointLight(255, 255, 255, x1, y1, z1);
+  translate(x1, y1, z1);
+  sphere(5);
+  pop();
+
+  push();
+  let x2 = 300 * cos(frameCount);
+  let y2 = 400 * cos(frameCount * 2);
+  let z2 = 500 * cos(frameCount * 0.5);
+  pointLight(0, 0, 0, x2, y2, z2);
+  translate(x2, y2, z2);
+  sphere(5);
+  pop();
+
+  ortho();
+  rotateX(radians(45));
+  // rotateX(radians(frameCount));
+  rotateY(radians(35));
+
+  specularMaterial(255);
+  for (var k = -floor(width * 0.08); k < floor(width * 0.08); k += 20) {
+    for (var i = -floor(width * 0.08); i < floor(width * 0.08); i += 20) {
+      for (
+        var j = -Math.ceil(width * 0.08);
+        j < Math.ceil(width * 0.08);
+        j += 20
+      ) {
+        let d = dist(0, 0, 0, i, j, k);
+        push();
+        // specularMaterial(map(d, 0, 220, 0, 100), 100, 50, 80);
+        translate(
+          i + i * 1.5 * sin(frameCount * 2 + d),
+          j + j * 1.5 * sin(frameCount * 1 + d),
+          k + k * 1.5 * sin(frameCount * 0.5 + d)
+        );
+        sphere(10, 30, 30);
+        pop();
+      }
+    }
   }
-  endShape(CLOSE);
 }
