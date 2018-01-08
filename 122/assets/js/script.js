0e66cf6b6e9ca45dce379d1be11c8691cc8ed0c5
a-y-u-s-h <code.ayush@gmail.com>
2018-01-08 22:12:53 +0530

    Add Exp. 122

--- 122/assets/js/script.js
+++ 122/assets/js/script.js
@@ -1,157 +1,53 @@
 let data = {
   sketch: {
-    background: "#000000",
-    frameRate: 60
+    background: "#1D1D1D"
   },
-  snake: {
-    size: 10,
-    fill: "#53D921",
-    stroke: {
-      color: "#00550A",
-      weight: 1
-    }
-  },
-  food: {
-    n: 30,
-    types: [
-      {
-        color: "#DBB822",
-        probability_of_showing_up: 0.2,
-        score: 3
-      },
-      {
-        color: "#DB1BA6",
-        probability_of_showing_up: 0.5,
-        score: 2
-      },
-      {
-        color: "#258197",
-        probability_of_showing_up: 0.7,
-        score: 1
-      },
-      {
-        color: "#BD1A1A",
-        probability_of_showing_up: 0.4,
-        score: -2
-      }
-    ]
-  },
-  grid: {
-    color: "#0B150B"
+  torus: {
+    R: window.innerWidth * 0.4,
+    r: window.innerWidth * 0.32
   }
 };
-
-// Variable to store GUI
-var controlkit;
-
-// Function to create control GUI
-var createControlKit = () => {
-  controlkit = new ControlKit();
-  controlkit
-    .addPanel({
-      fixed: true,
-      align: "right",
-      label: "Game Settings"
-    })
-    .addColor(data.grid, "color", {
-      colorMode: "hex",
-      label: "Grid Color"
-    })
-    .addNumberInput(data.snake, "size", {
-      label: "Grid Size",
-      step: 1,
-      dp: 1
-    })
-    .addNumberInput(data.sketch, "frameRate", {
-      label: "Framerate",
-      step: 1,
-      dp: 1
-    })
-    .addSubGroup({
-      label: "Snake Settings"
-    })
-    .addColor(data.snake, "fill", {
-      colorMode: "hex",
-      label: "Fill Color"
-    })
-    .addColor(data.snake.stroke, "color", {
-      colorMode: "hex",
-      label: "Stroke Color"
-    })
-    .addNumberInput(data.snake.stroke, "weight", {
-      label: "Border Thickness",
-      step: 1,
-      dp: 1
-    });
-};
-
-createControlKit();
-
-let snake;
-let grid;
-let foods = [];
-
-function init_food(snake, grid) {
-  for (var i = 0, upperLimit_i = data.food.n; i < upperLimit_i; i += 1) {
-    foods.push(new Food(snake, grid));
-    foods[i].initialize();
-  }
-}
 
 function setup() {
-  createCanvas(windowWidth * 0.77, windowHeight);
-  grid = new Grid();
-  snake = new Snake();
-  background(color(data.sketch.background));
-  init_food(snake, grid);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  colorMode(RGB, 100);
 }
 
-let angle = 0;
 function draw() {
-  frameRate(abs(data.sketch.frameRate));
-  background(0, 100);
-  let spacingX =
-    (width - Math.floor(width / data.snake.size) * data.snake.size) * 0.5;
-  let spacingY =
-    (height - Math.floor(height / data.snake.size) * data.snake.size) * 0.5;
-  translate(spacingX, spacingY);
-  grid.show();
-  snake.update();
-  snake.show();
-  for (var i = 0, upperLimit_i = foods.length; i < upperLimit_i; i += 1) {
-    if (foods[i]) {
-      foods[i].update();
-      foods[i].show();
+  background(data.sketch.background);
 
-      if (foods[i].cx == snake.cx && foods[i].cy == snake.cy) {
-        snake.eats(foods[i]);
-        foods.splice(i, 1);
-        foods[i] = new Food(snake, grid);
-        foods[i].initialize();
-      }
+  // ortho();
+  orbitControl();
+  translate(0, 0, -400);
+  rotateX(radians(90));
+  push();
+  let lx =
+    (data.torus.R * 0.25 + data.torus.r * cos(frameCount)) * cos(frameCount + 45);
+  let ly =
+    (data.torus.R * 0.25 + data.torus.r * cos(frameCount)) * sin(frameCount + 45);
+  let lz = 0;
+  pointLight(80, 80, 80, lx, ly, lz * cos(frameCount));
+  pointLight(80, 80, 80, lx, ly, lz * sin(frameCount));
+  pop();
+  /**
+   * Coordinates of a point in torus are parametrically defined as : 
+   * x (theta, phi) = (R + r * cos(theta)) * cos(phi)
+   * y (theta, phi) = (R + r * cos(theta)) * sin(phi)
+   * x (theta, phi) = r * sin(theta)
+   */
+  for (var phi = -180; phi < 180; phi += 10) {
+    for (var theta = -180; theta < 180; theta += 10) {
+      let x = data.torus.r * cos(theta + frameCount * 0.5) * cos(phi);
+      let y = data.torus.r * cos(theta + frameCount * 0.5) * sin(phi);
+      let z = data.torus.r * sin(theta + frameCount * 0.5);
+      let d = dist(x, z, y, 0, 0, 0);
+      push();
+      translate(data.torus.R * cos(phi), data.torus.R * sin(phi), 0);
+      translate(x, y, z);
+      scale(map(d, 0, 800, 0.01, 4));
+      sphere(20);
+      pop();
     }
   }
-}
-
-function keyPressed() {
-  if (mouseX < width) {
-    switch (keyCode) {
-      case UP_ARROW:
-        snake.dir(0, -1);
-        break;
-      case DOWN_ARROW:
-        snake.dir(0, 1);
-        break;
-      case LEFT_ARROW:
-        snake.dir(-1, 0);
-        break;
-      case RIGHT_ARROW:
-        snake.dir(1, 0);
-        break;
-    }
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth * 0.77, windowHeight);
 }
