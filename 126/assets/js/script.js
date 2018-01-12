ab84a1273f43a0e85b2ba50c67ad861ffd7cda62
a-y-u-s-h <code.ayush@gmail.com>
2018-01-12 22:19:31 +0530

    Add Exp. 126

--- 126/assets/js/script.js
+++ 126/assets/js/script.js
@@ -1,52 +1,44 @@
 let data = {
   sketch: {
-    background: "#FFFFFF"
-  },
-  tree: {
-    n : 14,
-    root: {
-      x: window.innerWidth * 0.5,
-      y: window.innerHeight * 0.5
-    },
-    node: {
-      size: 100,
-      textSize: 40,
-      textColor: "#FFFFFF",
-      stroke: {
-        check: true,
-        color: "#000000"
-      },
-      fill: {
-        check: true,
-        color: "#000000"
-      }
-    },
-    branch: {
-      r: 150,
-      angle: {
-        left: 45,
-        right: 45
-      }
-    }
+    background: "#000000"
   }
 };
 
-let tree;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
+  noFill();
+  stroke(255);
   angleMode(DEGREES);
-
-  tree = new Tree(width * 0.5, height * 0.1);
-  for (var i = 0, upperLimit_i = data.tree.n ; i < upperLimit_i; i += 1 ) {
-    tree.put(round(random(0, 100)));
-  }
+  colorMode(HSL, 100);
 }
 
 function draw() {
   background(data.sketch.background);
-  tree.connect();
-  tree.traverse();
+
+  translate(width * 0.5, height * 0.5);
+
+  for (var i = 30; i > 0; i -= 1) {
+    let frequency = 10;
+    let amplitude = 25;
+    push();
+    ellipse(0, 0, i * amplitude * 2, i * amplitude * 2);
+    fill(map(i, 60, 0, 90, 60), 100, 50, 1);
+    if (i > 1) {
+      let limit = i * 3;
+      for (var j = 1; j < limit + 1; j += 1) {
+        let phase = j * 360 / limit;
+
+        let x1 = (i - 1) * amplitude * sin(frequency + phase);
+        let y1 = (i - 1) * amplitude * cos(frequency + phase);
+        let x2 = i * amplitude * sin(frequency + phase);
+        let y2 = i * amplitude * cos(frequency + phase);
+
+        push();
+        rotate(frameCount * 0.01 * map(limit, 0, 30, 20, 0));
+        line(x1, y1, x2, y2);
+        pop();
+      }
+    }
+  }
+  pop();
 }
