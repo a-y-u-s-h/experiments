942dec3362b385ebffaa6bb25fc41a4868c210bb
a-y-u-s-h <code.ayush@gmail.com>
2018-02-02 01:57:06 +0530

    Modify Exp. 126

--- 126/assets/js/script.js
+++ 126/assets/js/script.js
@@ -1,6 +1,18 @@
 let data = {
   sketch: {
     background: "#000000"
+  },
+  constants: {
+    frequency: 20,
+    amplitude: 25
+  },
+  variables: {
+    limit: undefined,
+    phase: undefined,
+    x1: undefined,
+    y1: undefined,
+    x2: undefined,
+    y2: undefined
   }
 };
 
@@ -17,25 +29,23 @@ function draw() {
 
   translate(width * 0.5, height * 0.5);
 
-  for (var i = 30; i > 0; i -= 1) {
-    let frequency = 10;
-    let amplitude = 25;
+  for (var i = 20; i > 0; i -= 1) {
+    data.constants.amplitude = 40 * map(i, 0, 20, 0.5, 1);
     push();
-    ellipse(0, 0, i * amplitude * 2, i * amplitude * 2);
-    fill(map(i, 60, 0, 90, 60), 100, 50, 1);
+    ellipse(0, 0, i * data.constants.amplitude * 2, i * data.constants.amplitude * 2);
+    fill(map(i * sin(frameCount + i * 5), -20, 20, 0, 100), 100, 50, 1);
     if (i > 1) {
-      let limit = i * 3;
-      for (var j = 1; j < limit + 1; j += 1) {
-        let phase = j * 360 / limit;
-
-        let x1 = (i - 1) * amplitude * sin(frequency + phase);
-        let y1 = (i - 1) * amplitude * cos(frequency + phase);
-        let x2 = i * amplitude * sin(frequency + phase);
-        let y2 = i * amplitude * cos(frequency + phase);
+       data.variables.limit = map(i, 0, 20, 0, 100);
+      for (var j = 1; j < data.variables.limit + 1; j += 1) {
+         data.variables.phase = j * 360 / data.variables.limit;
+         data.variables.x1 = (i - 1) * 40 * map(i - 1, 0, 20, 0.5, 1) * sin(data.constants.frequency + data.variables.phase);
+         data.variables.y1 = (i - 1) * 40 * map(i - 1, 0, 20, 0.5, 1) * cos(data.constants.frequency + data.variables.phase);
+         data.variables.x2 = i * data.constants.amplitude * sin(data.constants.frequency + data.variables.phase);
+         data.variables.y2 = i * data.constants.amplitude * cos(data.constants.frequency + data.variables.phase);
 
         push();
-        rotate(frameCount * 0.01 * map(limit, 0, 30, 20, 0));
-        line(x1, y1, x2, y2);
+        rotate(frameCount * 0.015 * map(data.variables.limit, 0, 30, 10, 0));
+        line(data.variables.x1, data.variables.y1, data.variables.x2, data.variables.y2);
         pop();
       }
     }
