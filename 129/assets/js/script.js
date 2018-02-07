651c8a2ae96ecf8fe0a15313923e94afc505b260
a-y-u-s-h <code.ayush@gmail.com>
2018-02-07 23:45:04 +0530

    Add Exp. 129

--- 129/assets/js/script.js
+++ 129/assets/js/script.js
@@ -1,23 +1,56 @@
 let data = {
   sketch: {
-    background: "#248B21"
+    background: "#00000010"
+  },
+  animation: {
+    size: 20
   }
 };
 
-let ground;
-let left_paddle;
-let right_paddle;
+let comic = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  left_paddle = new Paddle("left");
-  right_paddle = new Paddle("right");
-  ground = new Ground(width * 0.5, height * 0.5, left_paddle);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  angleMode(DEGREES);
+  colorMode(HSL, 100);
 }
 
 function draw() {
-  background(data.sketch.background);
-  ground.show();
-  left_paddle.show();
-  right_paddle.show();
+  background(100);
+
+  translate(width * 0.5, height * 0.5);
+  if (comic) {
+    stroke(0);
+  } else {
+    noStroke();
+  }
+
+  for (
+    var x = -width * 0.5;
+    x < width * 0.5 + data.animation.size * 3;
+    x += data.animation.size
+  ) {
+    for (
+      var y = -height * 0.5;
+      y < height * 0.5 + data.animation.size * 3;
+      y += data.animation.size
+    ) {
+      let d = abs(dist(x, y, 0, 0));
+      push();
+      fill(map(d, 0, (width + height) * 0.5, 0, 100), 100, 40, 80);
+      rect(
+        x,
+        y,
+        data.animation.size * (1 + 0.5 * sin(-frameCount * 10 + d * 0.7)),
+        data.animation.size * (1 + 0.5 * sin(-frameCount * 10 + d * 0.7))
+      );
+      pop();
+    }
+  }
+}
+
+function mousePressed() {
+  comic = !comic;
 }
