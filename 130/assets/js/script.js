483a0b79b18523c33995820968411dba19e27753
Ayush Sharma <code.ayush@gmail.com>
2018-02-09 04:20:41 +0530

    Add Exp. 130

--- 130/assets/js/script.js
+++ 130/assets/js/script.js
@@ -1,52 +1,76 @@
 let data = {
   sketch: {
-    background: "#FFFFFF"
-  },
-  tree: {
-    n : 14,
-    root: {
-      x: window.innerWidth * 0.5,
-      y: window.innerHeight * 0.5
-    },
-    node: {
-      size: 100,
-      textSize: 40,
-      textColor: "#FFFFFF",
-      stroke: {
-        check: true,
-        color: "#000000"
-      },
-      fill: {
-        check: true,
-        color: "#000000"
-      }
-    },
-    branch: {
-      r: 150,
-      angle: {
-        left: 45,
-        right: 45
-      }
-    }
+    background: "#FFFFFF01"
   }
 };
 
-let tree;
+let ants = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ellipseMode(CENTER);
-  rectMode(CENTER);
   angleMode(DEGREES);
+  colorMode(HSL, 100);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  noStroke();
 
-  tree = new Tree(width * 0.5, height * 0.1);
-  for (var i = 0, upperLimit_i = data.tree.n ; i < upperLimit_i; i += 1 ) {
-    tree.put(round(random(0, 100)));
+  for (var y = 0; y < height; y += 3) {
+    ants.push(new Ant(random(width), y));
   }
 }
 
 function draw() {
   background(data.sketch.background);
-  tree.connect();
-  tree.traverse();
+
+  for (var i = 0; i < ants.length; i += 1) {
+    ants[i].update();
+    ants[i].show();
+  }
+}
+
+class Ant {
+  constructor(x, y, i = 0) {
+    this.position = new p5.Vector(x, y);
+    this.size = 5;
+    this.i = i;
+  }
+
+  show() {
+    push();
+    translate(this.position.x, this.position.y);
+    fill(
+      map(
+        this.position.x,
+        0,
+        width,
+        10 + 5 * map(this.i, 0, ants.length, 1, 2) * cos(frameCount + 10 * noise(this.position.x, this.position.y)),
+        50 + 50 * map(this.i, 0, ants.length, 0.5, 1) * sin(frameCount + 10 * noise(this.position.x, this.position.y))
+      ),
+      100,
+      50
+    );
+    ellipse(0, 0, this.size, this.size);
+    pop();
+  }
+
+  update() {
+    if (this.position.x > width + this.size) {
+      this.position.x = -this.size;
+    }
+
+    if (this.position.x < -this.size) {
+      this.position.x = width + this.size;
+    }
+
+    if (this.position.y > height + this.size) {
+      this.position.y = -this.size;
+    }
+
+    if (this.position.y < -this.size) {
+      this.position.y = height + this.size;
+    }
+    this.position.x += 10 * noise(this.position.x, this.position.y) * sin(frameCount * map(this.i, 0, ants.length, 0, 2 * noise(this.i, this.position.x)));
+    this.position.y += 10 * noise(this.position.x, this.position.y) * cos(frameCount * map(this.i, 0, ants.length, 0, 2 * noise(this.i, this.position.x)));
+    this.position.x += 1;
+  }
 }
