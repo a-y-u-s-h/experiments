0b5ae3dda9ec605f37e89a7716cec1490afd5f38
Ayush Sharma <code.ayush@gmail.com>
2018-02-18 05:27:21 +0530

    Add Exp. 136 modified

--- 136/assets/js/script.js
+++ 136/assets/js/script.js
@@ -25,3 +25,10 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   grid = new Grid(width * 0.6, height * 0.8, data.grid.scale, width * 0.35);
 }
+
+function mousePressed() {
+  if (mouseX < width * 0.65 && mouseX > width * 0.05 && mouseY < height * 0.9 && mouseY > 0.1 * height ) {
+    background(data.sketch.background);
+    grid = new Grid(width * 0.6, height * 0.8, data.grid.scale, width * 0.35);
+  }
+}
