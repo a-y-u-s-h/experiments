235d92d1ab98fdaf76bb0bdb108dd92866afa978
Ayush Sharma <code.ayush@gmail.com>
2018-03-13 23:56:21 +0530

    Add Exp. 146

--- 146/assets/js/script.js
+++ 146/assets/js/script.js
@@ -1,157 +1,66 @@
 let data = {
-  sketch: {
-    background: "#000000",
-    frameRate: 60
-  },
-  snake: {
-    size: 10,
-    fill: "#53D921",
-    stroke: {
-      color: "#00550A",
-      weight: 1
+    sketch: {
+        background: "#000000"
+    },
+    cwave: {
+        r: 200,
+        thickness: 5
     }
-  },
-  food: {
-    n: 30,
-    types: [
-      {
-        color: "#DBB822",
-        probability_of_showing_up: 0.2,
-        score: 3
-      },
-      {
-        color: "#DB1BA6",
-        probability_of_showing_up: 0.5,
-        score: 2
-      },
-      {
-        color: "#258197",
-        probability_of_showing_up: 0.7,
-        score: 1
-      },
-      {
-        color: "#BD1A1A",
-        probability_of_showing_up: 0.4,
-        score: -2
-      }
-    ]
-  },
-  grid: {
-    color: "#0B150B"
-  }
 };
 
-// Variable to store GUI
-var controlkit;
-
-// Function to create control GUI
-var createControlKit = () => {
-  controlkit = new ControlKit();
-  controlkit
-    .addPanel({
-      fixed: true,
-      align: "right",
-      label: "Game Settings"
-    })
-    .addColor(data.grid, "color", {
-      colorMode: "hex",
-      label: "Grid Color"
-    })
-    .addNumberInput(data.snake, "size", {
-      label: "Grid Size",
-      step: 1,
-      dp: 1
-    })
-    .addNumberInput(data.sketch, "frameRate", {
-      label: "Framerate",
-      step: 1,
-      dp: 1
-    })
-    .addSubGroup({
-      label: "Snake Settings"
-    })
-    .addColor(data.snake, "fill", {
-      colorMode: "hex",
-      label: "Fill Color"
-    })
-    .addColor(data.snake.stroke, "color", {
-      colorMode: "hex",
-      label: "Stroke Color"
-    })
-    .addNumberInput(data.snake.stroke, "weight", {
-      label: "Border Thickness",
-      step: 1,
-      dp: 1
-    });
-};
-
-createControlKit();
-
-let snake;
-let grid;
-let foods = [];
-
-function init_food(snake, grid) {
-  for (var i = 0, upperLimit_i = data.food.n; i < upperLimit_i; i += 1) {
-    foods.push(new Food(snake, grid));
-    foods[i].initialize();
-  }
-}
+let r;
+let g;
+let b;
 
 function setup() {
-  createCanvas(windowWidth * 0.77, windowHeight);
-  grid = new Grid();
-  snake = new Snake();
-  background(color(data.sketch.background));
-  init_food(snake, grid);
+    createCanvas(windowWidth, windowHeight);
+    strokeWeight(40);
+    ellipseMode(CENTER);
+    rectMode(CENTER);
+    angleMode(DEGREES);
+    r = new CWave(width * 0.5, height * 0.5, 100, 0);
+    g = new CWave(width * 0.5, height * 0.5, 200, 120);
+    b = new CWave(width * 0.5, height * 0.5, 300, 270);
 }
 
-let angle = 0;
 function draw() {
-  frameRate(abs(data.sketch.frameRate));
-  background(0, 100);
-  let spacingX =
-    (width - Math.floor(width / data.snake.size) * data.snake.size) * 0.5;
-  let spacingY =
-    (height - Math.floor(height / data.snake.size) * data.snake.size) * 0.5;
-  translate(spacingX, spacingY);
-  grid.show();
-  snake.update();
-  snake.show();
-  for (var i = 0, upperLimit_i = foods.length; i < upperLimit_i; i += 1) {
-    if (foods[i]) {
-      foods[i].update();
-      foods[i].show();
-
-      if (foods[i].cx == snake.cx && foods[i].cy == snake.cy) {
-        snake.eats(foods[i]);
-        foods.splice(i, 1);
-        foods[i] = new Food(snake, grid);
-        foods[i].initialize();
-      }
-    }
-  }
-}
-
-function keyPressed() {
-  if (mouseX < width) {
-    switch (keyCode) {
-      case UP_ARROW:
-        snake.dir(0, -1);
-        break;
-      case DOWN_ARROW:
-        snake.dir(0, 1);
-        break;
-      case LEFT_ARROW:
-        snake.dir(-1, 0);
-        break;
-      case RIGHT_ARROW:
-        snake.dir(1, 0);
-        break;
-    }
-  }
+    background(data.sketch.background);
+    blendMode(ADD);
+    r.show("#C33B3B90");
+    b.show("#14ABCD90");
+    g.show("#24CC5D90");
+    blendMode(NORMAL);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth * 0.77, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+class CWave {
+    constructor(cx, cy, r, phase) {
+        this.position = new p5.Vector(cx, cy);
+        this.r = r;
+        this.phase = phase;
+    }
+
+    show(color = "#FFFFFF") {
+        push();
+        stroke(color);
+        translate(this.position.x, this.position.y);
+        for (var i = -180, upperLimit_i = 180; i < upperLimit_i; i += 2) {
+            let x = this.r * cos(i + this.phase) + 100 * noise(random(1, -1), random(1, -1)) * map(i, -180, 180, -1, 1) * cos(frameCount + i * 0.1);
+            let y = this.r * sin(i + this.phase) + 100 * noise(random(1, -1), random(1, -1)) * sin(frameCount * 3);
+            push();
+            if (i > 0) {
+                this.r = data.cwave.r + 50 * sin(frameCount * 2 + i * 3);
+                strokeWeight(map(i, 0, 180, 0, 30 * (1 + sin(-frameCount * 3 + i))));
+            } else {
+                this.r = data.cwave.r + 50 * sin(-frameCount * 2 + i * 3);
+                strokeWeight(map(i, 0, -180, 0, 30 * (1 + sin(frameCount * 3 + i))));
+            }
+            point(x, y);
+            pop();
+        }
+        pop();
+    }
 }
