1921628eea4f4c3dd87850cee91a0097886e139c
Ayush Sharma <code.ayush@gmail.com>
2018-03-14 23:24:51 +0530

    Add Exp. 147

--- 147/assets/js/script.js
+++ 147/assets/js/script.js
@@ -1,81 +1,71 @@
-var gl;
-var shaderProgram;
-var vertices;
-var x;
-let width;
-let height;
-initGL();
-createShaders();
-createVertices();
-draw();
+let creatures = [];
 
-function initGL() {
-  let canvas = document.getElementById("canvas");
-  gl = canvas.getContext("webgl");
-  width = canvas.width = window.innerWidth;
-  height = canvas.height = window.innerHeight;
-  gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(1, 1, 1, 1);
-}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  angleMode(DEGREES);
 
-function createShaders() {
-
-  let vertex_shader_source = `
-
-  attribute vec4 coords;
-  attribute float pointSize;
-
-  void main (void) {
-    gl_Position = vec4(coords);
-    gl_PointSize = pointSize;
+  for (var i = 0, upperLimit_i = 400 ; i < upperLimit_i; i += 1 ) {
+    creatures.push( new Being(random(width), random(height)));
   }
-  `;
-
-  let vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertex_shader, vertex_shader_source);
-  gl.compileShader(vertex_shader);
-  let fragment_shader_source = `
-  precision mediump float;
-  uniform vec4 color;
-    void main (void) {
-      gl_FragColor = color;
-    }
-  `;
-
-  let fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragment_shader, fragment_shader_source);
-  gl.compileShader(fragment_shader);
-
-  shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertex_shader);
-  gl.attachShader(shaderProgram, fragment_shader);
-  gl.linkProgram(shaderProgram);
-  gl.useProgram(shaderProgram);
+  noFill();
 }
 
 function draw() {
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
+  background(255);
+  stroke(0);
+  creatures.forEach((creature) => {
+    creature.run()
+  });
 }
 
-function createVertices () {
+class Being {
+  constructor(x, y) {
+    this.position = new p5.Vector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.acceleration = new p5.Vector();
+    this.size = random(50, 150);
+    this.phase = random(360);
+    this.direction = random([-1, 1]);
+    this.standard_deviation = random(0.01, 0.08);
+  }
 
-  vertices = [
-  -0.9, -0.9, 0.0,
-  0.9, -0.9, 0.0,
-  0.0, 0.9, 0.0
-  ];
+  run()  {
+    this.update();
+    this.show();
+  }
 
-  var buffer = gl.createBuffer();
-  gl.bindbuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new float32Array(vertices), gl.STATIC_DRAW);
+  show() {
+    push();
+    translate(this.position.x, this.position.y);
+    beginShape();
+    for (var i = 0, upperLimit_i = 20; i < upperLimit_i; i += 1) {
+      let x = this.size * (1 + 0.5 * randomGaussian(1, this.standard_deviation)) * sin(this.phase + i + frameCount * this.direction);
+      let y = this.size * (1 + 0.5 * randomGaussian(1, this.standard_deviation)) * cos(this.phase + i + frameCount * this.direction);
+      let d = dist(x, y, this.position.x, this.position.y);
+      strokeWeight(1);
+      curveVertex(x, y);
+      // point(x, y);
+    }
+    endShape();
+    pop();
+  }
 
-  var coords = gl.getAttribLocation(shaderProgram, "coords");
-  var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
-  gl.vertexAttrib1f(pointSize, 10);
+  update() {
+    if (this.position.x >= width || this.position.x < 0) {
+      this.velocity.x *= -1;
+    }
 
-  var color = gl.getUniformLocation(shaderProgram, "color");
-  gl.uniform4f(color, 0.2, 0.3, 0.1, 1);
+    if (this.position.y >= height || this.position.y <= 0) {
+      this.velocity.y *= -1;
+    }
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
 }
-
-
