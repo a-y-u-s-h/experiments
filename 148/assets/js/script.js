8d54966bf9d8039c449947044cb90ccdecd7235f
Ayush Sharma <code.ayush@gmail.com>
2018-03-16 23:57:01 +0530

    Add Exp. 148 - Final commit

--- 148/assets/js/script.js
+++ 148/assets/js/script.js
@@ -1,9 +1,9 @@
 let data = {
   sketch: {
-    background: "#FFFFFF"
+    background: "#000000"
   },
   sphere: {
-    r: 100,
+    r: 250,
     scale: 10
   }
 }
@@ -19,6 +19,7 @@ function setup() {
 function draw() {
   background(data.sketch.background);
   orbitControl();
+  scale(0.7)
   rotateX(45);
   rotateY(-15);
   rotateX(30);
@@ -30,17 +31,17 @@ function draw() {
   pop();
   // rotateZ(frameCount);
 
-  for (var theta = 0, upperLimit_theta = 360 ; theta < upperLimit_theta; theta += 5 ) {
-    for (var phi = 0, upperLimit_phi = 180 ; phi < upperLimit_phi; phi += 10 ) {
+  for (var theta = 0, upperLimit_theta = 360 ; theta < upperLimit_theta; theta += 8 ) {
+    for (var phi = 0, upperLimit_phi = 180 ; phi < upperLimit_phi; phi += 8 ) {
       let d = dist(theta, phi, 0, 0);
-      let x = data.sphere.r * sin(theta) * sin(phi)  + noise(d) * 2 * sin(frameCount * 2);
-      let y = data.sphere.r * cos(theta) * sin(phi)  + noise(d) * 100 * tan(frameCount * 0.5);
-      let z = data.sphere.r * cos(phi) + random(10);
+      let x = data.sphere.r * sin(theta) * sin(phi)  + noise(d) * 100 * sin(frameCount * 2) + tan(frameCount * 0.1 + d) * 4;
+      let y = data.sphere.r * cos(theta) * sin(phi)  + noise(d) * 100 * cos(frameCount * 2) + tan(frameCount * 0.1 + d) * 4;
+      let z = data.sphere.r * cos(phi);
       push();
       translate(x, y, z);
       fill(map(theta, 0, 360, 0, 100), 100, 100, 100);
       rotateX(phi + frameCount);
-      sphere(2);
+      sphere(10);
       pop();
     }
   }
