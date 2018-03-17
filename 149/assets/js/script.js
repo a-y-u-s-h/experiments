9b40ff45d1b167707aeda08385c7d13856e1f4b7
Ayush Sharma <code.ayush@gmail.com>
2018-03-17 23:32:45 +0530

    Add Exp. 149

--- 149/assets/js/script.js
+++ 149/assets/js/script.js
@@ -1,23 +1,74 @@
 let data = {
   sketch: {
-    background: "#248B21"
+    background: "#111111"
+  },
+  grid: {
+    n: {
+      x: 40,
+      y: 30
+    },
+    scale: {
+      x: null,
+      y: null
+    }
   }
 };
 
-let ground;
-let left_paddle;
-let right_paddle;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  left_paddle = new Paddle("left");
-  right_paddle = new Paddle("right");
-  ground = new Ground(width * 0.5, height * 0.5, left_paddle);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  data.grid.n.x *= random(0.3, 1.4);
+  data.grid.n.y *= random(0.3, 1.4);
+  data.grid.scale.x = width / data.grid.n.x;
+  data.grid.scale.y = height / data.grid.n.y;
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  fill(255);
+  colorMode(HSB, 100);
+  angleMode(DEGREES);
+  noStroke();
 }
+
+let factor = 1;
 
 function draw() {
   background(data.sketch.background);
-  ground.show();
-  left_paddle.show();
-  right_paddle.show();
+  orbitControl();
+  rotateX(10);
+  translate(0, 0, -300);
+
+  translate(-width * 0.5, -height * 0.5);
+
+  for (var x = data.grid.scale.x * 0.5; x < width; x += data.grid.scale.x) {
+    for (var y = data.grid.scale.y * 0.5; y < height; y += data.grid.scale.y) {
+      push();
+      let d = dist(x, y, width * 0.5, height * 0.5);
+      fill(map(d, 0, (width + height) * 0.5, 0, 100), 100, 90, 100);
+      translate(
+        x,
+        y,
+        400 * (1 - 0.9 * noise(x, y) * tan(factor * frameCount * noise(x, y)))
+      );
+      scale(1 - 0.9 * noise(x, y) * sin(frameCount * noise(x, y)));
+      rect(0, 0, data.grid.scale.y * 0.9, data.grid.scale.y * 0.9);
+      pop();
+    }
+  }
+}
+
+function mousePressed () {
+  factor *= -1;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  data.grid.n.x *= random(0.3, 1.4);
+  data.grid.n.y *= random(0.3, 1.4);
+  data.grid.scale.x = width / data.grid.n.x;
+  data.grid.scale.y = height / data.grid.n.y;
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  fill(255);
+  colorMode(HSB, 100);
+  angleMode(DEGREES);
+  noStroke();
 }
