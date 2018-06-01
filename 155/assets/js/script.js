05810a133762a6590749b28d108aa9fde69c6a92
Ayush Sharma <code.ayush@gmail.com>
2018-06-02 01:01:30 +0530

    Finish Exp. 155 completely

--- 155/assets/js/script.js
+++ 155/assets/js/script.js
@@ -2,7 +2,7 @@ let data = {
     sketch: {
         background: "#000000"
     },
-    hexaskelion: {
+    skelion: {
         position: {
             x: 0,
             y: 0
@@ -11,6 +11,10 @@ let data = {
         radius: 100,
         theta: 590,
         a: 0.986,
+        animate: {
+            a: true,
+            b: false
+        },
         b: 0.411,
         rotation_speed_factor: 1
     },
@@ -20,6 +24,10 @@ let data = {
             color: "#FFFFFF",
             weight: 2
         }
+    },
+    shape: {
+        n: 6,
+        animate: false
     }
 }
 
@@ -41,27 +49,41 @@ var createControlKit = () => {
         .addSubGroup({
             label: "Hexaskelion Settings"
         })
-        .addNumberInput(data.hexaskelion, "theta", {
+        .addNumberInput(data.skelion, "theta", {
             label: "Subpart length",
             step: 1,
             dp: 3
         })
-        .addNumberInput(data.hexaskelion, "a", {
-            label: "Hexaskelion factor 'a'",
+        .addNumberInput(data.skelion, "a", {
+            label: "Factor A",
             step: 0.01,
             dp: 3
         })
-        .addNumberInput(data.hexaskelion, "b", {
-            label: "Hexaskelion factor 'b'",
+        .addCheckbox(data.skelion.animate, "a", {
+            label: "Animate A?"
+        })
+        .addValuePlotter(data.skelion, "a", {
+            label: "A's variation",
+            height: 65
+        })
+        .addNumberInput(data.skelion, "b", {
+            label: "Factor B",
             step: 0.001,
             dp: 3
         })
-        .addNumberInput(data.hexaskelion, "scale", {
+        .addCheckbox(data.skelion.animate, "b", {
+            label: "Animate B?"
+        })
+        .addValuePlotter(data.skelion, "b", {
+            label: "Factor b",
+            height: 65,
+        })
+        .addNumberInput(data.skelion, "scale", {
             label: "Scale Factor",
             step: 1,
             dp: 3
         })
-        .addNumberInput(data.hexaskelion, "radius", {
+        .addNumberInput(data.skelion, "radius", {
             label: "Radius",
             step: 1,
             dp: 3
@@ -78,72 +100,84 @@ var createControlKit = () => {
             step: 1,
             dp: 3
         })
-        .addValuePlotter(data.hexaskelion, "a", {
-            label: "a",
-            height: 50,
+        .addCheckbox(data.shape, "animate", {
+            label: "Animate Shape?"
+        })
+        .addNumberInput(data.shape, "n", {
+            label: "Vertices",
+            step: 1,
+            dp: 3
         })
 };
+createControlKit();
 
-let hexy;
+let shapy;
 
 function setup() {
     createCanvas(windowWidth * 0.77, windowHeight);
-    data.hexaskelion.position.x = width * 0.5; 
-    data.hexaskelion.position.y = height * 0.5;
-    hexy = new Hexaskelion(data.hexaskelion.position.x, data.hexaskelion.position.y);
+    data.skelion.position.x = width * 0.5;
+    data.skelion.position.y = height * 0.5;
+    shapy = new Skelion(data.skelion.position.x, data.skelion.position.y);
     angleMode(DEGREES);
     noFill();
 }
 
 function draw() {
     background(data.sketch.background);
-    hexy.show();
-    hexy.update();
+    if (data.skelion.animate.a) {
+        data.skelion.a = 0.5 + 0.49 * sin(frameCount);
+    }
+    if (data.skelion.animate.b) {
+        data.skelion.b = 0.5 + 0.49 * sin(frameCount);
+    }
+    controlkit.update();
+    shapy.show();
+    shapy.update();
 }
 
-class Hexaskelion {
+class Skelion {
     constructor(x, y) {
         this.position = new p5.Vector(x, y);
         this.velocity = new p5.Vector();
         this.acceleration = new p5.Vector();
     }
 
-    update()  {
-        this.position.x = data.hexaskelion.position.x;
-        this.position.y = data.hexaskelion.position.y;
+    update() {
+        this.position.x = data.skelion.position.x;
+        this.position.y = data.skelion.position.y;
     }
 
     show() {
         push();
         translate(this.position.x, this.position.y);
-        scale(data.hexaskelion.scale);
-        rotate(frameCount * data.hexaskelion.rotation_speed_factor);
+        scale(data.skelion.scale);
+        rotate(frameCount * data.skelion.rotation_speed_factor);
         if (data.appearance.stroke.check) {
             strokeWeight(data.appearance.stroke.weight);
             stroke(data.appearance.stroke.color);
         } else {
             noStroke();
         }
-        for (let theta = 0; theta < 360; theta += 60) {
-            let rx = data.hexaskelion.radius * cos(theta);
-            let ry = data.hexaskelion.radius * sin(theta);
-            push();
-            translate(rx, ry);
-            rotate(theta);
-            beginShape();
-            let x;
-            let y;
-            for (let i = 0; i < data.hexaskelion.theta; i += 1) {
-                let r = data.hexaskelion.a * Math.exp(radians(i * data.hexaskelion.b));
-                x = r * cos(i);
-                y = r * sin(i);
-                curveVertex(x, y);
+        if (data.shape.n > 0) {
+            for (let theta = 0; theta < 360; theta += 360 / data.shape.n) {
+                let rx = data.skelion.radius * cos(theta);
+                let ry = data.skelion.radius * sin(theta);
+                push();
+                translate(rx, ry);
+                rotate(theta);
+                beginShape();
+                let x;
+                let y;
+                for (let i = 0; i < data.skelion.theta; i += 1) {
+                    let r = data.skelion.a * Math.exp(radians(i * data.skelion.b));
+                    x = r * cos(i);
+                    y = r * sin(i);
+                    curveVertex(x, y);
+                }
+                endShape();
+                pop();
             }
-            endShape();
-            pop();
         }
         pop();
     }
 }
-
-createControlKit();
